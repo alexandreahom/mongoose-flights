2889fb2fa0d2620d0ca257b3da7fc39a997156ee
Alexandrea <lexhom24@gmail.com>
2022-06-21 20:20:22 -0400

    add stylesheets

--- models/flight.js
+++ models/flight.js
@@ -3,8 +3,29 @@ import mongoose from "mongoose"
 const Schema = mongoose.Schema
 	
 const flightSchema = new Schema({
-  airline: String,
-  airport: String,
-  flightNo: Number,
-  departs: Date,
+  airline: {
+    type: String,
+    enum: ['American', 'Southwest', 'United']
+  },
+  airport: {
+    type: String,
+    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']
+  },
+  flightNo: {
+    type: Number,
+    min: 10,
+    max: 9999,
+    required: true
+  },
+  departs: {
+    type: Date,
+    default: function() {
+      const today = new Date()
+      const oneYear = today.getFullYear() + 1
+      today.setFullYear(oneYear)
+      return today
+    }
+  }
+}, {
+  timestamps: true
 })
